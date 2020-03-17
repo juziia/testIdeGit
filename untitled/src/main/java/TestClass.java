5119c6e490a4283311e3906e65eb096928c1986a
juzia <1943622045@qq.com>
2020-03-17 20:36:40 +0800

    测试提交

--- untitled/src/main/java/TestClass.java
+++ untitled/src/main/java/TestClass.java
@@ -16,9 +16,11 @@ public class TestClass {
         System.out.println(".....................");
         Integer num = 20;
         num += 20 + 20 +20;
+        // 测试git拉取代码
     }
 
     public void test(){
+        // 测试git提交代码
         Integer i = 10;
         i += 20+20;
         System.out.println("-------------------------------");
